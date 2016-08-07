f694769fc0fd92875258c50e2da1140ecd8129b6
ulrich <git@myvdr.de>
2016-08-07 21:50:16 +0200

    fix 7edec52d031d7162a164e18faaa35cd53f0395c7 set contentType application/json | jquerys default is urlencoded use JSON.stringify() for POST data use .click event handler use eventData to transfer the data to the callback function http://api.jquery.com/click/#click-eventData-handler

--- muell.js
+++ muell.js
@@ -1,13 +1,14 @@
 var muell_url = "http://openhab.shack/muellshack/";
 for (bid in {"gelber_sack":"","restmuell":"","papiermuell":""}){
-  $( "#"+bid+"-img" ).on("click",function (){
+  $( "#"+bid+"-img" ).click({url: muell_url + bid},function (event){
       $.ajax({
         type: "POST",
-        url: muell_url + bid,
-        data: {"main_action_done":true} ,
+        url: event.data.url,
+        data: JSON.stringify({"main_action_done":true}) ,
         success: function (data) {
             alert("Thanks for making the shackspace a cleaner hackerspace")
         },
+        contentType: "application/json; charset=UTF-8",
         dataType: "json"
       });
       $(this).css("visibility","hidden")
